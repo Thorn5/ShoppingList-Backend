7812381815251f5d22e07143f7b4fd75dbd8ad04
Trygve Horn <trygvehorn@gmail.com>
2025-01-12 17:51:01 +0100

    JSON Queries updated

--- src/server/ShoppingList_Server.mjs
+++ src/server/ShoppingList_Server.mjs
@@ -1,8 +1,9 @@
 // ShoppingList_Server.mjs
 
-import express from 'express';
-import pkg from 'pg';
-import { config } from 'dotenv';
+import express from "express";
+import cors from "cors";
+import pkg from "pg";
+import { config } from "dotenv";
 
 config();
 
@@ -19,126 +20,152 @@ const pool = new Pool({
   port: process.env.POSTGRES_PORT,
 });
 
-app.get('/', async (req, res) => {
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.get("/", async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT NOW()');
+    const result = await client.query("SELECT NOW()");
     res.send(result.rows);
     client.release();
   } catch (err) {
     console.error(err);
-    res.send('Error ' + err);
+    res.send("Error " + err);
   }
 });
 
-app.get('/aisles', async (req, res) => {
+app.get("/aisles", async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM aisles');
+    const result = await client.query("SELECT * FROM aisles");
     res.json(result.rows);
     client.release();
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error ' + err);
+    res.status(500).send("Error " + err);
   }
 });
 
-app.get('/products', async (req, res) => {
+app.get("/products", async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM products');
+    const result = await client.query("SELECT * FROM products");
     res.json(result.rows);
     client.release();
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error ' + err);
+    res.status(500).send("Error " + err);
   }
 });
 
-app.get('/shops', async (req, res) => {
+app.get("/shops", async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM shops');
+    const result = await client.query("SELECT * FROM shops");
     res.json(result.rows);
     client.release();
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error ' + err);
+    res.status(500).send("Error " + err);
   }
 });
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM users');
+    const result = await client.query("SELECT * FROM users");
     res.json(result.rows);
     client.release();
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error ' + err);
+    res.status(500).send("Error " + err);
   }
 });
 
-app.get('/lists', async (req, res) => {
+app.get("/lists", async (req, res) => {
   try {
     const client = await pool.connect();
-
-    const query = `
-        SELECT json_agg(
-          json_build_object(
-            'user_id', u.user_id,
-            'first_name', u.first_name,
-            'last_name', u.last_name,
-            'email', u.email,
-            'shopping_lists', (
+    const jsonQueryAllUsers = `
+  SELECT json_agg(
+    json_build_object(
+      'user_id', u.user_id,
+      'first_name', u.first_name,
+      'last_name', u.last_name,
+      'email', u.email,
+      'shopping_lists', (
+      SELECT json_agg(
+        json_build_object(
+          'list_shop_id', sls.list_shop_id,
+          'shop_name', s.shop_name,
+          'website', s.website,
+          'aisles', (
+          SELECT json_agg(
+            json_build_object(
+              'aisle_id', a.aisle_id,
+              'name', a.name,
+              'description', a.description,
+              'products', (
               SELECT json_agg(
                 json_build_object(
-                  'list_shop_id', sls.list_shop_id,
-                  'shop_name', s.shop_name,
-                  'address', s.address,
-                  'contact_number', s.contact_number,
-                  'website', s.website,
-                  'aisles', (
-                    SELECT json_agg(
-                      json_build_object(
-                        'aisle_id', a.aisle_id,
-                        'name', a.name,
-                        'description', a.description,
-                        'products', (
-                          SELECT json_agg(
-                            json_build_object(
-                              'product_id', p.product_id,
-                              'name', p.name,
-                              'notes', lap.notes,
-                              'quantity', lap.quantity,
-                              'price', lap.price
-                            )
-                          )
-                          FROM list_aisle_products lap
-                          JOIN products p ON lap.product_id = p.product_id
-                          WHERE lap.list_shop_id = lsa.list_shop_id AND lap.aisle_id = lsa.aisle_id
-                        )
-                      )
-                    )
-                    FROM list_shop_aisles lsa
-                    JOIN aisles a ON lsa.aisle_id = a.aisle_id
-                    WHERE lsa.list_shop_id = sls.list_shop_id
-                  )
+                  'product_id', p.product_id,
+                  'name', p.name,
+                  'notes', lap.notes,
+                  'quantity', lap.quantity,
+                  'price', lap.price
                 )
               )
-              FROM shopping_list_shops sls
-              JOIN shops s ON sls.shop_id = s.shop_id
-              WHERE sls.user_id = u.user_id
+                    FROM list_aisle_products lap
+                    JOIN products p ON lap.product_id = p.product_id
+                    WHERE lap.list_shop_id = lsa.list_shop_id AND lap.aisle_id = lsa.aisle_id
             )
           )
-        ) AS users_data
-        FROM users u;
-      `;
-
-    const result = await client.query(query);
-
+        )
+              FROM list_shop_aisles lsa
+              JOIN aisles a ON lsa.aisle_id = a.aisle_id
+              WHERE lsa.list_shop_id = sls.list_shop_id
+      )
+    )
+  )
+        FROM shopping_list_shops sls
+        JOIN shops s ON sls.shop_id = s.shop_id
+        WHERE sls.user_id = u.user_id
+      ),
+    'todo_lists', (
+      SELECT json_agg(
+        json_build_object(
+          'list_id', tl.list_id,
+          'name', tl.name,
+          'description', tl.description,
+          'entries', (
+          SELECT json_agg(
+            json_build_object(
+              'entry_id', te.entry_id,
+              'name', te.name,
+              'description', te.description,
+              'due_date', te.due_date,
+              'status', te.status
+            )
+          )
+              FROM todo_entries te
+              WHERE te.list_id = tl.list_id
+        )
+      )
+        )
+        FROM todo_lists tl
+        WHERE tl.user_id = u.user_id
+      )
+    )
+  ) AS users_data
+  FROM users u;
+  `;
+    console.log(jsonQueryAllUsers);
+    const result = await client.query(jsonQueryAllUsers);
     if (result.rows.length === 0 || !result.rows[0].users_data) {
-      res.status(404).send('No users or shopping lists found');
+      res.status(404).send("No users or shopping lists found");
     } else {
       res.json(result.rows[0].users_data);
     }
@@ -146,16 +173,15 @@ app.get('/lists', async (req, res) => {
     client.release();
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error: ' + err);
+    res.status(500).send("Error: " + err);
   }
 });
 
-app.get('/lists/:id', async (req, res) => {
+app.get("/lists/:id", async (req, res) => {
   try {
     const client = await pool.connect();
-
-    const query = `
-SELECT json_build_object(
+    const jsonQuerySingleUser = `
+    SELECT json_build_object(
       'user_id', u.user_id,
       'first_name', u.first_name,
       'last_name', u.last_name,
@@ -165,8 +191,6 @@ SELECT json_build_object(
           json_build_object(
             'list_shop_id', sls.list_shop_id,
             'shop_name', s.shop_name,
-            'address', s.address,
-            'contact_number', s.contact_number,
             'website', s.website,
             'aisles', (
               SELECT json_agg(
@@ -199,16 +223,39 @@ SELECT json_build_object(
         FROM shopping_list_shops sls
         JOIN shops s ON sls.shop_id = s.shop_id
         WHERE sls.user_id = u.user_id
+      ),
+      'todo_lists', (
+        SELECT json_agg(
+          json_build_object(
+            'list_id', tl.list_id,
+            'name', tl.name,
+            'description', tl.description,
+            'entries', (
+              SELECT json_agg(
+                json_build_object(
+                  'entry_id', te.entry_id,
+                  'name', te.name,
+                  'description', te.description,
+                  'due_date', te.due_date,
+                  'status', te.status
+                )
+              )
+              FROM todo_entries te
+              WHERE te.list_id = tl.list_id
+            )
+          )
+        )
+        FROM todo_lists tl
+        WHERE tl.user_id = u.user_id
       )
     ) AS user_data
     FROM users u
     WHERE u.user_id = $1;
-`;
-
-    const result = await client.query(query, [req.params.id]);
-
+    `;
+    console.log(jsonQuerySingleUser);
+    const result = await client.query(jsonQuerySingleUser, [req.params.id]);
     if (result.rows.length === 0) {
-      res.status(404).send('User not found');
+      res.status(404).send("User not found");
     } else {
       res.json(result.rows[0].user_data);
     }
@@ -216,7 +263,7 @@ SELECT json_build_object(
     client.release();
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error: ' + err);
+    res.status(500).send("Error: " + err);
   }
 });
 
